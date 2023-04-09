95c437bcaf066b837848a6c6bbc1686a983f2d24
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-04-09 20:53:40 +0200

    Navbar

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('#navbar');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const navLinks = document.querySelectorAll('.nav-link');
+
+// Toggle navigation menu on small screens
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('navbar-mobile');
+});
+
+// Close navigation menu when a link is clicked on small screens
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('navbar-mobile');
+    });
+});
