8f6d47b70306879d4362755406300a12e9c908a2
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-04-22 15:55:31 +0200

    Added the home page to symfony

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,8 @@
  */
 import './logo2.png'
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/style.css';
 
+import './main';
 // start the Stimulus application
 import './bootstrap';
