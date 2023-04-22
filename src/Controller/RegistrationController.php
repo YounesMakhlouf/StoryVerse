a8dbf1d86cc2f672f04935d3dd54aa6215d98af2
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-04-22 23:12:00 +0100

    Added verifyUserEmail to registration controller

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,15 +12,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use App\Repository\UserRepository;
+
 
 class RegistrationController extends AbstractController
 {
-    
     #[Route('/register', name: 'app_register')]
     public function register(Request $request,
     MailerService $mailer,
      UserPasswordHasherInterface $userPasswordHasher,
-      EntityManagerInterface $entityManager): Response
+      EntityManagerInterface $entityManager,
+      VerifyEmailHelperInterface $verifyEmailHelper): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -36,8 +39,32 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
+            $signatureComponents = $verifyEmailHelper->generateSignature('app_verify_email',
+        $user->getId(),
+        $user->getEmail(),
+        ['id' => $user->getId()]);
+        $link=$signatureComponents->getSignedUrl();
+        $username=$user->getUsername();
+        $body="<div>Hello {$username},</div>
+        <div>Thanks for your interest in creating an account.
+        To create your account, please verify your email address by clicking below.</div>
+        <button style ='{
+            background-color: #008CBA ;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+        }'
+          onclick='window.location.href={$link}';>
+      Click Here
+    </button>";
+        $mailer->sendEmail($to=$user->getEmail(),$content=$body);
+
             // do anything else you need here, like send an email
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_verify_email',array('user' => $user));
         }
 
         return $this->render('registration/register.html.twig', [
@@ -45,7 +72,22 @@ class RegistrationController extends AbstractController
         ]);
     }
     #[Route("/verify",name:'app_verify_email')]
-    public function verifyUserEmail(){
+    public function verifyUserEmail(Request $request,
+     VerifyEmailHelperInterface $verifyEmailHelper,
+     UserRepository $userRepository,
+     $user)
+     : Response
+     {
+        try {
+            $verifyEmailHelper->validateEmailConfirmation(
+                $request->getUri(),
+                $user->getId(),
+                $user->getEmail(),
+            );
+        } catch (VerifyEmailExceptionInterface $e) {
+            $this->addFlash('error', $e->getReason());
+            return $this->redirectToRoute('app_register');
+        }
 
     }
 }
