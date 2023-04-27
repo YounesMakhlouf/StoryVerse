3792b53d2cf726a807e651c436adeedf1ba28d67
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-04-27 13:46:30 +0200

    Added profile page Deleted kedma 9dima

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('login', './assets/js/login.js')
+    .addEntry('profile', './assets/js/profile.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
