bf1b41b0a662bfd3215d9b622f90b1a816b3cb48
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-04-28 22:02:38 +0100

    Add user fixture for generating test data

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\DataFixtures;
+use App\Factory\UserFactory;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        UserFactory::createMany(25);
+
+        $manager->flush();
+    }
+}
