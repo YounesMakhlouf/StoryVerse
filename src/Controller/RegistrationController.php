f6f0fde93a681803e7d80add6e27eb64961f7241
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-04-28 18:14:03 +0100

    Added findUserbyId and findUserbyEmai to the UserReepository

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -76,7 +76,7 @@ class RegistrationController extends AbstractController
     {
 
         try {
-            $user = $this->userRepository->findOneBy(['id' => $id]);
+            $user =  $this->userRepository->findUserById($id);
             $this->emailVerifier->handleEmailConfirmation($request, $user);
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
@@ -84,7 +84,7 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_register');
         }
 
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Your email address has been verified ! Please sign in to continue');
 
         return $this->redirectToRoute('app_login');
     }
@@ -96,7 +96,7 @@ class RegistrationController extends AbstractController
     ])]
 public function SendVerification($id,$resend='0',TemplatedEmail $email){
 
-    $user = $this->userRepository->findOneBy(['id' => $id]);
+    $user = $this->userRepository->findUserById($id);
     if(!$user){
         $this->addFlash('error', 'user not found.');
 
