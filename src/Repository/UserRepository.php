cca321e7b35a39eb0829540ba38d15fc6c33a3d8
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-04-28 18:14:03 +0100

    Added findUserbyId and findUserbyEmai to the UserReepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -71,13 +71,20 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?User
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+public function findUserById( $id): ?User
+{
+    return $this->createQueryBuilder('u')
+        ->andWhere('u.id = :id')
+        ->setParameter('id', $id)
+        ->getQuery()
+        ->getOneOrNullResult();
+}
+public function findUserByEmail( $email): ?User
+{
+    return $this->createQueryBuilder('u')
+        ->andWhere('u.email = :email')
+        ->setParameter('email', $email)
+        ->getQuery()
+        ->getOneOrNullResult();
+}
 }
