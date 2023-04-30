01c0f03db55a15ccc3bdea6398691b039ab65487
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-04-30 01:43:05 +0100

    Added DashboardController

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
