c8027b841faf83b88fd3084a4edb15bc05eb676e
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-05-01 04:39:52 +0100

    Added story fixture for generating test data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\DataFixtures;
+use App\Factory\StoryFactory;
 use App\Factory\UserFactory;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -12,6 +13,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         UserFactory::createMany(25);
+        StoryFactory::createMany(25);
 
         $manager->flush();
     }
