d3956015ba1c3a311d8850041117e84532cee9a0
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-05-01 19:26:17 +0200

    Aana navbar mezyena (kifek ya zin ^^)

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,7 @@
 import '../styles/home.scss';
 
 import './components/AOSInit';
-import './components/navigation';
+import './components/navigation2';
 
 // start the Stimulus application
 import '../bootstrap';
