dfcbe39553732cb16169197ece97ca8281611d3d
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-05-02 22:18:20 +0100

    Added StatController & added chart to admin Dashboard

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,2 +1,29 @@
 import '../styles/admin.scss';
 import '../bootstrap';
+import'../img/logo.webp';
+import {Chart} from "chart.js";
+ let url='/chart-data';
+ fetch(url).then(response => response.json())
+     .then(
+        data=> {
+            console.log(data);
+          const labels=data.labels;
+          const counts=data.datasets.data;
+         const ctx = document.getElementById('gender_chart').getContext('2d');
+         const chart = new Chart(ctx, {
+          type: 'pie',
+          data: {
+           labels: labels,
+           datasets: [{
+            data: counts,
+            backgroundColor:data.datasets.backgroundColor
+           }]
+          }
+         });
+        })
+     .catch(error => console.error(error));
+
+
+
+
+
