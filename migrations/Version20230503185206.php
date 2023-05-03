c6fc60a55112040504b8e04572e9572bae4712bd
WindyJasmin <yassmine.riahi@insat.ucar.tn>
2023-05-03 22:20:13 +0100

    ahla competetions w ahla windy Jasmin

--- migrations/Version20230503185206.php
+++ migrations/Version20230503185206.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230501180129 extends AbstractMigration
+final class Version20230503185206 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,16 @@ final class Version20230501180129 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE genre ADD slug VARCHAR(100) NOT NULL');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_835033F8989D9B62 ON genre (slug)');
+        $this->addSql('ALTER TABLE story DROP FOREIGN KEY FK_EB560438F675F31B');
+        $this->addSql('DROP INDEX IDX_EB560438F675F31B ON story');
+        $this->addSql('ALTER TABLE story DROP author_id');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_835033F8989D9B62 ON genre');
-        $this->addSql('ALTER TABLE genre DROP slug');
+        $this->addSql('ALTER TABLE story ADD author_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE story ADD CONSTRAINT FK_EB560438F675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_EB560438F675F31B ON story (author_id)');
     }
 }
