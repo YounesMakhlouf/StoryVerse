e2155756f6c831fbb4f6224755af09b94a763e9b
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-05-04 21:16:20 +0100

    Moved email verification logic out of the registration controller

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 use App\Factory\ContributionFactory;
 use App\Factory\StoryFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,8 +12,9 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        StoryFactory::createMany(25);
-        ContributionFactory::createMany(100);
+//        StoryFactory::createMany(25);
+        UserFactory::createMany(50);
+//        ContributionFactory::createMany(100);
         $manager->flush();
     }
 }
