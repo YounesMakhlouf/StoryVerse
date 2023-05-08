5512c898d7444e078b15046a252ee784efd38d16
WindyJasmin <yassmine.riahi@insat.ucar.tn>
2023-05-08 11:20:00 +0100

    the form to create a competition

--- src/Controller/CompetitionController.php
+++ src/Controller/CompetitionController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
+
+namespace App\Controller;
+
+use App\Entity\Competition;
+use App\Form\CompetitionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompetitionController extends AbstractController
 {
-    #[Route('/competition', name: 'app_competition')]
-    public function index(): Response
+    #[Route('/competition/create', name: 'competition_create')]
+    public function createCompetition(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $competition = new Competition();
+        $form = $this->createForm(CompetitionType::class, $competition);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $competition = $form->getData();
+
+            // handle file upload
+            $imageFile = $form->get('imageFile')->getData();
+            if ($imageFile) {
+                $newFilename = uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('competition_images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // handle the exception if something goes wrong
+                }
+
+                $competition->setImageFilename($newFilename);
+            }
+
+            $entityManager->persist($competition);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Competition created successfully!');
+            return $this->redirectToRoute('competition');
+        }
+
         return $this->render('competition/index.html.twig', [
-            'controller_name' => 'CompetitionController',
+            'form' => $form->createView(),
         ]);
     }
 }
