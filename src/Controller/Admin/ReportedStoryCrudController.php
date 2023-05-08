255d372750f5685ab9aae905e9dc53e5a99f847f
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-05-08 21:58:01 +0100

    Added accept report action

--- src/Controller/Admin/ReportedStoryCrudController.php
+++ src/Controller/Admin/ReportedStoryCrudController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Admin;
 use Doctrine\ORM\Query\FilterCollection;
 use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
@@ -20,7 +22,13 @@ class ReportedStoryCrudController extends StoryCrudController
     public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, \EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection $filters): QueryBuilder
     {
         return parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters)
-            ->andWhere('entity.isReported = :report')
+            ->andWhere('entity.IsReported = :report')
             ->setParameter('report', true);
+    }
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action){
+                return $action->SetLabel('Accept Report');});
     }
 }
