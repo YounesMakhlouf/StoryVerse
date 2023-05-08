bd00b75b86df6670397dcfb005c21a63da19f0ad
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-05-08 23:34:18 +0100

    Added deprive admin role action

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -11,6 +16,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -19,6 +25,33 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $addAdmin= Action::new('Add as admin')
+            ->addCssClass('btn btn-success')
+            ->setIcon('fa-solid fa-user-plus')
+            ->displayAsButton()
+            ->linkToCrudAction('addAdmin')
+            ->setTemplatePath('admin/add_admin.html.twig')
+            ->displayIf(static function (User $user): bool {
+                return !(in_array("ROLE_ADMIN",$user->getRoles()));
+            });
+
+        $removeAdmin= Action::new('Deprive Admin Role')
+            ->addCssClass('btn btn-danger')
+            ->setIcon('fa-solid fa-user-minus')
+            ->displayAsButton()
+            ->linkToCrudAction('removeAdmin')
+            ->setTemplatePath('admin/add_admin.html.twig')
+            ->displayIf(static function (User $user): bool {
+                return (in_array("ROLE_ADMIN",$user->getRoles()));
+            });
+        return parent::configureActions($actions)
+            ->add(Crud::PAGE_INDEX, $addAdmin)
+            ->add(Crud::PAGE_INDEX, $removeAdmin);
+
+    }
 
     public function configureFields(string $pageName): iterable
     {
@@ -50,6 +83,32 @@ class UserCrudController extends AbstractCrudController
                 ->setSortable(false),
 
         ];
+    }
+    public function addAdmin(AdminContext $adminContext, EntityManagerInterface $entityManager,AdminUrlGenerator $adminUrlGenerator)
+    {
+        $user = $adminContext->getEntity()->getInstance();
+        $user->setRoles(["ROLE_ADMIN"]);
+        $entityManager->persist($user);
+        $entityManager->flush();
+        $targetUrl = $adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Crud::PAGE_DETAIL)
+            ->setEntityId($user->getId())
+            ->generateUrl();
+        return $this->redirect($targetUrl);
+    }
+    public function removeAdmin(AdminContext $adminContext, EntityManagerInterface $entityManager,AdminUrlGenerator $adminUrlGenerator)
+    {
+        $user = $adminContext->getEntity()->getInstance();
+        $user->setRoles(["ROLE_USER"]);
+        $entityManager->persist($user);
+        $entityManager->flush();
+        $targetUrl = $adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Crud::PAGE_DETAIL)
+            ->setEntityId($user->getId())
+            ->generateUrl();
+        return $this->redirect($targetUrl);
     }
 
 }
