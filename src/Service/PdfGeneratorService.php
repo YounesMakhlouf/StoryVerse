0ba7eb73e4684410b75070bbc5069c3377c8b3ee
Selma Bouabidi <selma.bouabidi@insat.ucar.tn>
2023-05-08 14:59:22 +0100

    Implemented pdf generator service

--- src/Service/PdfGeneratorService.php
+++ src/Service/PdfGeneratorService.php
@@ -2,7 +2,22 @@
 
 namespace App\Service;
 
+use Dompdf\Dompdf;
+
 class PdfGeneratorService
 {
+    private Dompdf $domPdf;
+    public function __construct() {
+        $this->domPdf = new DomPdf();
+    }
+
+    public function GeneratePdf($html): void
+    {
+        $this->domPdf->loadHtml($html);
+        $this->domPdf->render();
+        $this->domPdf->stream("Story.pdf", [
+            'Attachment' => true
+        ]);
+    }
 
 }
