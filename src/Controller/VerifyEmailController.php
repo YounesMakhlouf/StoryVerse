a2b1b8188ee38199f39631a9e61054606fde4f86
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-05-30 21:01:48 +0200

    Added exception handling to edge cases in verifyEmailController

--- src/Controller/VerifyEmailController.php
+++ src/Controller/VerifyEmailController.php
@@ -4,57 +4,57 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use App\security\EmailVerifier;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class VerifyEmailController extends AbstractController
 {
-    private UserRepository $userRepository;
-    private EmailVerifier $emailVerifier;
-
-    public function __construct(UserRepository $userRepository, EmailVerifier $emailVerifier)
+    public function __construct(private readonly UserRepository $userRepository, private readonly EmailVerifier $emailVerifier)
     {
-        $this->userRepository = $userRepository;
-        $this->emailVerifier = $emailVerifier;
     }
 
-
+    /**
+     * @throws NonUniqueResultException
+     */
     #[Route('/verify/email/{id}', name: 'app_verify_email')]
-public function verifyUserEmail(Request $request, TranslatorInterface $translator, $id): Response
-{
-    try {
+    public function verifyUserEmail(Request $request, TranslatorInterface $translator, $id): Response
+    {
         $user = $this->userRepository->findUserById($id);
-        $this->emailVerifier->handleEmailConfirmation($request, $user);
-    } catch (VerifyEmailExceptionInterface $exception) {
-        $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
-
-        return $this->redirectToRoute('app_register');
-    }
-
-    $this->addFlash('success', 'Your email address has been verified ! Please sign in to continue');
-
-    return $this->redirectToRoute('app_login');
-}
-
-
-
-
-
-    #[Route('/sendEmail/{id}/{resend}', name: 'app_send_verification_email', requirements: [
-        'id' => '^[1-9]\d*$',
-        'resend' => '[01]'
-    ])]
-
-    public function SendVerification( $id, $resend = '0'):Response
-    {$user = $this->userRepository->findUserById($id);
         if (!$user) {
             $this->addFlash('error', 'user not found.');
+            throw new HttpException(404, 'User not found');
+        }
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $user);
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+            return $this->redirectToRoute('app_register');
+        }
 
+        $this->addFlash('success', 'Your email address has been verified! Please sign in to continue.');
+        return $this->redirectToRoute('app_login');
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     */
+    #[Route('/sendEmail/{id}/{resend}', name: 'app_send_verification_email', requirements: [
+        'id' => '\d+',
+        'resend' => '[01]'
+    ])]
+    public function SendVerification($id, $resend = '0'): Response
+    {
+        $user = $this->userRepository->findUserById($id);
+        if (!$user) {
+            $this->addFlash('error', 'user not found.');
+            throw new HttpException(404, 'User not found');
         } else {
             if ($user->isIsVerified()) {
                 $this->addFlash('success', 'Your email is already verified ! ');
@@ -63,15 +63,14 @@ public function verifyUserEmail(Request $request, TranslatorInterface $translato
                 $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                     (new TemplatedEmail())
                         ->to($user->getEmail())
-                        ->subject('Please Confirm your Email'), ['id' => $id]
+                        ->subject('Please Confirm your Email')
+                        ->context(['id' => $id])
                 );
                 if ($resend == '1') {
-
                     $this->addFlash('info', 'A new verification email has been sent to your email address.');
                 }
-
             }
+            return $this->render('registration/verification.html.twig', ['id' => $id]);
         }
-        return $this->render('registration/verification.html.twig', ['id' => $id]);
     }
 }
