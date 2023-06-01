81769ce01da39d1083ed00ee14770e23ab8c6a1c
YounesMakhlouf <younes.makhlouf@insat.ucar.tn>
2023-06-01 11:24:42 +0100

    Improved storyPage logic

--- src/Controller/Story/ReportStoryController.php
+++ src/Controller/Story/ReportStoryController.php
@@ -2,27 +2,29 @@
 
 namespace App\Controller\Story;
 
+use App\Entity\Story;
 use App\Repository\StoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ReportStoryController extends AbstractController
 {
+    #[IsGranted('ROLE_USER')]
     #[Route('/report/story/{id}', name: 'app_report_story')]
-    public function index($id , StoryRepository $storyRepository,EntityManagerInterface $entityManager): Response
+    public function index($id, StoryRepository $storyRepository, EntityManagerInterface $entityManager): Response
     {
-        $story=$storyRepository->find($id);
+        $story = $storyRepository->find($id);
+
+        if (!$story instanceof Story) {
+            throw $this->createNotFoundException('Story not found');
+        }
+
         $story->setIsReported(true);
         $entityManager->persist($story);
         $entityManager->flush();
-        return $this->json([
-
-            'message'=>'Story reported successfully'
-        ]);
-
-
-
+        return $this->json(['message' => 'Story reported successfully']);
     }
 }
--- src/Controller/Story/StorypageController.php
+++ src/Controller/Story/StorypageController.php
@@ -17,108 +17,115 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class StorypageController extends AbstractController
-{   #[IsGranted('ROLE_USER')]
-    #[Route('/storypage/{id}', name: 'app_storypage')]
-    public function index( Request $request,$id,StoryRepository $storyRepository,EntityManagerInterface $entityManager): Response
+{
+    public function __construct(private readonly EntityManagerInterface $entityManager, private readonly StoryRepository $storyRepository)
     {
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/storypage/{id}', name: 'app_storypage')]
+    public function index(Request $request, int $id): Response
+    {
+        $story = $this->findStoryById($id);
+
         $comment = new Comment();
         $form = $this->createForm(CommentType::class, $comment);
-        $story=$storyRepository->find($id);
-        $contribution=new Contribution();
-        $ContributionForm = $this->createForm(ContributionType::class, $contribution);
+
+        $contribution = new Contribution();
+        $contributionForm = $this->createForm(ContributionType::class, $contribution);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-          return $this->addComment($comment,$story,$entityManager);
+            return $this->addComment($comment, $story, $this->entityManager);
 
         }
-        $ContributionForm->handleRequest($request);
+        $contributionForm->handleRequest($request);
 
-        if ($ContributionForm->isSubmitted()) {
-            return $this->addContribution($story,$entityManager,$contribution);
+        if ($contributionForm->isSubmitted()) {
+            return $this->addContribution($story, $contribution);
         }
 
+        $hasContributed = $this->hasContributed($story);
+        $hasLiked = $story->getLikes()->contains($this->getUser());
 
-
-        $hasContributed=$this->hasContributed($entityManager,$story);
-        $hasLiked=$story->getLikes()->contains($this->getUser());
         return $this->render('storypage/story.html.twig', [
-            'controller_name' => 'CompletedStoryController',
-           'story'=>$story,
-            'hasLiked'=>$hasLiked,
-            'hasContributed'=>$hasContributed,
+            'story' => $story,
+            'hasLiked' => $hasLiked,
+            'hasContributed' => $hasContributed,
             'form' => $form->createView(),
-            'contributionForm'=>$ContributionForm->createView()
+            'contributionForm' => $contributionForm->createView()
         ]);
     }
 
-    #[Route('/storypage/like/{id}', name: 'app_like')]
-
-    public function addLike($id,Request $request, EntityManagerInterface $entityManager,StoryRepository $storyRepository): JsonResponse
+    private function findStoryById(int $id): Story
     {
-        $story=$storyRepository->find($id);
-        $user=$this->getUser();
-        if($story->getLikes()->contains($user)){
-            $story->getLikes()->removeElement($user);
+        $story = $this->storyRepository->find($id);
 
+        if (!$story instanceof Story) {
+            throw $this->createNotFoundException('Story not found');
         }
-        else{
-            $story->addLike($user);
-        }
-        $entityManager->persist($story);
-        $entityManager->flush();
-        return $this->json([
-            'count' => $story->getlikes()->count()
-        ]);
 
+        return $story;
     }
 
-    public function addContribution(Story $story, EntityManagerInterface $entityManager, Contribution $contribution): JsonResponse
-    {
-        $contribution->setStory($story);
-        $contribution->setAuthor($this->getUser());
-        $contribution->setPosition($story->getContributions()->count()+1);
-        $entityManager->persist($contribution);
-        $entityManager->flush();
-        return $this->json([
-            'content' => $contribution->getContent(),
-            'id'=> $contribution->getAuthor()->getId(),
-        ]);
-    }
-
-    public function addComment(Comment $comment,Story $story,EntityManagerInterface $entityManager): JsonResponse
+    public function addComment(Comment $comment, Story $story, EntityManagerInterface $entityManager): JsonResponse
     {
         $comment->setAuthor($this->getUser());
         $comment->setStory($story);
         $entityManager->persist($comment);
         $entityManager->flush();
 
-//            // Return a JSON response with the new comment data
+        // Return a JSON response with the new comment data
         return $this->json([
             'content' => $comment->getContent(),
-            'author'=> $comment->getAuthor()->getUsername(),
+            'author' => $comment->getAuthor()->getUsername(),
             'createdAt' => $comment->getCreatedAt()->format('Y-m-d H:i:s'),
-            'count' =>$story->getComments()->count(),
-            'avatar'=>$comment->getAuthor()->getAvatar()
-
+            'count' => $story->getComments()->count(),
+            'avatar' => $comment->getAuthor()->getAvatar()
         ]);
     }
 
-    public function hasContributed(EntityManagerInterface $entityManager,Story $story){
+    public function addContribution(Story $story, Contribution $contribution): JsonResponse
+    {
+        $contribution->setStory($story);
+        $contribution->setAuthor($this->getUser());
+        $contribution->setPosition($story->getContributions()->count() + 1);
+        $this->entityManager->persist($contribution);
+        $this->entityManager->flush();
+        return $this->json([
+            'content' => $contribution->getContent(),
+            'id' => $contribution->getAuthor()->getId(),
+        ]);
+    }
+
+    private function hasContributed(Story $story): bool
+    {
         $user = $this->getUser();
-        $contributions = $entityManager->getRepository(Contribution::class)->findBy([
+        $contributions = $this->entityManager->getRepository(Contribution::class)->findBy([
             'author' => $user,
             'story' => $story,
         ]);
 
-        if (count($contributions) > 0) {
-            return true;
-        }
-         else {
-             return false;
-         }
+        return count($contributions) > 0;
     }
 
+    #[Route('/storypage/like/{id}', name: 'app_like')]
+    public function addLike(int $id): JsonResponse
+    {
+        $story = $this->findStoryById($id);
+        $user = $this->getUser();
 
+        if ($story->getLikes()->contains($user)) {
+            $story->removeLike($user);
+        } else {
+            $story->addLike($user);
+        }
 
+        $this->entityManager->persist($story);
+        $this->entityManager->flush();
+
+        return $this->json([
+            'count' => $story->getlikes()->count()
+        ]);
+    }
 }
