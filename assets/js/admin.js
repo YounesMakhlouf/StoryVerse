fcbcd3affe6868def5f09f457d4aa6c4e3513c48
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-06-02 15:11:46 +0200

    Fixed charts in the admin dashboard Extracted card statistics into their own template

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,43 +1,9 @@
 import '../styles/admin.scss';
 import '../bootstrap';
-import'../img/logo.webp';
-import {Chart} from "chart.js";
+import Chart from 'chart.js/auto';
 
-function CreateChart(url,id,colors,type,lengendVisibility){
-    fetch(url).then(response => response.json())
-        .then(
-            data=> {
-                console.log(data);
-                const labels=data.labels;
-                const counts=data.data;
-                const ctx = document.getElementById(id).getContext('2d');
-                const chart = new Chart(ctx, {
-                    type: type,
-                    data: {
-                        labels: labels,
-                        datasets: [{
-                            label: '',
-                            data: counts,
-                            backgroundColor:colors
-                        }]
-                    },
-                    options: {
-                        plugins: {
-                            legend: {
-                                display: lengendVisibility
-                            }
-                        }
-                    }
-
-                });
-            })
-        .catch(error => console.error(error));
-}
-
-
-
-let genderColors=["#89CFF0", "#f4c2c2"]
-let languageColors= [
+let genderColors = ["#89CFF0", "#f4c2c2"]
+let languageColors = [
     'rgb(255, 99, 132)',
     'rgb(255, 159, 64)',
     'rgb(255, 205, 86)',
@@ -45,7 +11,7 @@ let languageColors= [
     'rgb(54, 162, 235)',
     'rgb(153, 102, 255)',
     'rgb(201, 203, 207)']
-let genresColors=[
+let genresColors = [
     '#FF8C8C',
     '#FEE440',
     '#146C94',
@@ -54,7 +20,50 @@ let genresColors=[
     '#B2A4FF'
 ]
 
-CreateChart('/chart-data','gender_chart',genderColors,'pie',true);
-CreateChart('/language','language_chart',languageColors,'bar',false);
-CreateChart('/genre','genre_chart',genresColors,'bar',false);
+function fetchData(url) {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch chart data');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        });
+}
 
+function createChart(ctx, data, colors, type, legendVisibility) {
+    return new Chart(ctx, {
+        type: type,
+        data: {
+            labels: data.labels,
+            datasets: [
+                {
+                    label: '',
+                    data: data.data,
+                    backgroundColor: colors
+                }
+            ]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: legendVisibility
+                }
+            }
+        }
+    });
+}
+
+function initializeChart(url, id, colors, type, legendVisibility) {
+    const ctx = document.getElementById(id).getContext('2d');
+    fetchData(url)
+        .then(data => {
+            createChart(ctx, data, colors, type, legendVisibility);
+        });
+}
+
+initializeChart('/chart-data', 'gender_chart', genderColors, 'pie', true);
+initializeChart('/language', 'language_chart', languageColors, 'bar', false);
+initializeChart('/genre', 'genre_chart', genresColors, 'bar', false);
