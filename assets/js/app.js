95bccdfa09a1e196d3c77cf4ef2899978edc8f05
YounesMakhlouf <younes.makhlouf@insat.ucar.tn>
2023-06-15 12:54:47 +0100

    Fixed the home page header

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@
 import '../styles/app.scss';
 
 import './components/AOSInit';
-import './components/navigation';
+import './components/notifications';
 import Swal from "sweetalert2";
 
 // start the Stimulus application
