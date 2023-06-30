9445cc9d2ef0ecd92470f6be266fde8f2d00b8d4
YounesMakhlouf <younes.makhlouf@insat.ucar.tn>
2023-06-30 21:26:12 +0100

    Added leaderboard

---
+++ src/Controller/LeaderboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LeaderboardController extends AbstractController
+{
+    #[Route('/leaderboard', name: 'app_leaderboard')]
+    public function leaderboard(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findBy([], ['xp' => 'DESC']);
+    
+        return $this->render('leaderboard/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
