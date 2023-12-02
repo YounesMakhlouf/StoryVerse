70560fbba294ea7009a32eca2fb143d374e37e8b
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2023-12-02 16:47:25 +0100

    Added hover effect to the navbar for all pages using a simpler IntersectionObserver

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ require('bootstrap');
 
 import "./components/AOSInit";
 import "./components/notifications";
+import "./components/navigation_home";
 
 // start the Stimulus application
 import "../bootstrap";
--- assets/js/components/navigation_home.js
+++ assets/js/components/navigation_home.js
@@ -1,37 +1,50 @@
-const nav = document.querySelector("#navbar");
-let header = document.querySelector("#header");
+// const nav = document.querySelector("#navbar");
+// let header = document.querySelector(".primary-header");
+// const scroll
+// /**
+//  * Navbar links active state on scroll
+//  */
+// let navbarlinks = document.querySelectorAll("#navbar .scrollto");
+// const navbarlinksActive = () => {
+//     let position = window.scrollY + 200;
+//     navbarlinks.forEach((navbarlink) => {
+//         if (!navbarlink.hash) return;
+//         let section = document.querySelector(navbarlink.hash);
+//         if (!section) return;
+//         if (position >= section.offsetTop && position <= section.offsetTop + section.offsetHeight) {
+//             navbarlink.classList.add("active");
+//         } else {
+//             navbarlink.classList.remove("active");
+//         }
+//     });
+// };
+// window.addEventListener("load", navbarlinksActive);
+// window.addEventListener("scroll", navbarlinksActive);
+//
+// /**
+//  * Toggle .header-scrolled class to #header when page is scrolled
+//  */
+// if (header) {
+//     const headerScrolled = () => {
+//         if (window.scrollY > 100) {
+//             header.classList.add("header-scrolled");
+//         } else {
+//             header.classList.remove("header-scrolled");
+//         }
+//     };
+//     window.addEventListener("load", headerScrolled);
+//     document.addEventListener("scroll", headerScrolled);
+// }
 
-/**
- * Navbar links active state on scroll
- */
-let navbarlinks = document.querySelectorAll("#navbar .scrollto");
-const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    navbarlinks.forEach((navbarlink) => {
-        if (!navbarlink.hash) return;
-        let section = document.querySelector(navbarlink.hash);
-        if (!section) return;
-        if (position >= section.offsetTop && position <= section.offsetTop + section.offsetHeight) {
-            navbarlink.classList.add("active");
-        } else {
-            navbarlink.classList.remove("active");
-        }
-    });
-};
-window.addEventListener("load", navbarlinksActive);
-window.addEventListener("scroll", navbarlinksActive);
+const primaryHeader = document.querySelector(".primary-header");
+const scrollWatcher = document.createElement("div");
 
-/**
- * Toggle .header-scrolled class to #header when page is scrolled
- */
-if (header) {
-    const headerScrolled = () => {
-        if (window.scrollY > 100) {
-            header.classList.add("header-scrolled");
-        } else {
-            header.classList.remove("header-scrolled");
-        }
-    };
-    window.addEventListener("load", headerScrolled);
-    document.addEventListener("scroll", headerScrolled);
-}
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+primaryHeader.before(scrollWatcher);
+
+const navObserver =new IntersectionObserver((entries) =>{
+    console.log(entries);
+    primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting);
+}, {rootMargin: "200px 0px 0px 0px"});
+
+navObserver.observe(scrollWatcher);
