d90a8809e94ba1f071226af2bdd8b19784bf2179
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-08-15 14:06:39 +0100

    Finalizing

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,48 +1,64 @@
-import '../styles/admin.scss';
-import '../bootstrap';
-import Chart from 'chart.js/auto';
+import "../styles/admin.scss";
+import "../bootstrap";
+import Chart from "chart.js/auto";
 
-let genderColors = ["#89CFF0", "#f4c2c2"];
-let languageColors = ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)', 'rgb(201, 203, 207)'];
-let genresColors = ['#FF8C8C', '#FEE440', '#146C94', '#FFBF9B', '#54B435', '#B2A4FF'];
+const COLORS = {
+  gender: ["#89CFF0", "#f4c2c2"],
+  language: [
+    "rgb(255, 99, 132)",
+    "rgb(255, 159, 64)",
+    "rgb(255, 205, 86)",
+    "rgb(75, 192, 192)",
+    "rgb(54, 162, 235)",
+    "rgb(153, 102, 255)",
+    "rgb(201, 203, 207)",
+  ],
+  genre: ["#FF8C8C", "#FEE440", "#146C94", "#FFBF9B", "#54B435", "#B2A4FF"],
+};
 
 function fetchData(url) {
-    return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch chart data');
-            }
-            return response.json();
-        })
-        .catch(error => {
-            console.error('An error occurred:', error);
-        });
-}
-
-function createChart(ctx, data, colors, type, legendVisibility) {
-    return new Chart(ctx, {
-        type: type, data: {
-            labels: data.labels, datasets: [{
-                label: '', data: data.data, backgroundColor: colors
-            }]
-        }, options: {
-            plugins: {
-                legend: {
-                    display: legendVisibility
-                }
-            }
-        }
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch chart data");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
     });
 }
 
-function initializeChart(url, id, colors, type, legendVisibility) {
-    const ctx = document.getElementById(id).getContext('2d');
-    fetchData(url)
-        .then(data => {
-            createChart(ctx, data, colors, type, legendVisibility);
-        });
+function createChart(ctx, data, colors, type, legendVisibility) {
+  return new Chart(ctx, {
+    type: type,
+    data: {
+      labels: data.labels,
+      datasets: [
+        {
+          label: "",
+          data: data.data,
+          backgroundColor: colors,
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: legendVisibility,
+        },
+      },
+    },
+  });
 }
 
-initializeChart('/chart-data', 'gender_chart', genderColors, 'pie', true);
-initializeChart('/language', 'language_chart', languageColors, 'bar', false);
-initializeChart('/genre', 'genre_chart', genresColors, 'bar', false);
+function initializeChart(url, id, colors, type, legendVisibility) {
+  const ctx = document.getElementById(id).getContext("2d");
+  fetchData(url).then((data) => {
+    createChart(ctx, data, colors, type, legendVisibility);
+  });
+}
+
+initializeChart("/chart-data", "gender_chart", COLORS.gender, "pie", true);
+initializeChart("/language", "language_chart", COLORS.language, "bar", false);
+initializeChart("/genre", "genre_chart", COLORS.genre, "bar", false);
--- assets/js/components/notifications.js
+++ assets/js/components/notifications.js
@@ -2,32 +2,37 @@ const notificationList = document.querySelector(".notiflist");
 
 // Fetch notifications data from the server
 async function fetchNotifications() {
-    try {
-        const response = await fetch(`/notification/user`);
-        const data = await response.json();
+  try {
+    const response = await fetch(`/notification/user`);
+    const data = await response.json();
 
-        if (Array.isArray(data) && data.length > 0) {
-            data.forEach((notification) => {
-                displayNotification(notification);
-            });
-        } else {
-            displayNotification("No notifications yet, don't lose hope!");
-        }
-    } catch (error) {
-        console.error("Failed to fetch notifications:", error);
-        displayNotification("Failed to fetch notifications. Please try again later.");
+    if (Array.isArray(data) && data.length > 0) {
+      data.forEach((notification) => {
+        displayNotification(notification);
+      });
+    } else {
+      displayNotification("No notifications yet, don't lose hope!");
     }
+  } catch (error) {
+    console.error("Failed to fetch notifications:", error);
+    displayNotification(
+      "Failed to fetch notifications. Please try again later."
+    );
+  }
 }
 
 // Display a notification in the list
 function displayNotification(notification) {
-    const listItem = document.createElement("li");
+  const listItem = document.createElement("li");
+  listItem.classList.add("dropdown-item");
+
+  if (notification.sender) {
     const userLink = document.createElement("a");
-    userLink.href =`/profile/${notification.sender.id}`;
-    listItem.classList.add("dropdown-item");
+    userLink.href = `/profile/${notification.sender.id}`;
     listItem.textContent = notification.content;
     userLink.appendChild(listItem);
     notificationList.appendChild(userLink);
+  }
 }
 
 fetchNotifications();
